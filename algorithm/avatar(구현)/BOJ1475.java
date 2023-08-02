66156edf7e30f16520248e30057562c8b1a253ae
MaSeongJin <73871625+MaSeongJin@users.noreply.github.com>
2023-08-02 17:42:33 +0900

    Update BOJ1475.java

--- algorithm/avatar(구현)/BOJ1475.java
+++ algorithm/avatar(구현)/BOJ1475.java
@@ -1,16 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-		
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
         String s = br.readLine();
         // 카운팅하기 편하게 배열 길이 1증가
         int[] arr = new int[10];
@@ -39,5 +33,4 @@ public class Main {
         System.out.print(max);
 		
 	}
-	
 }
