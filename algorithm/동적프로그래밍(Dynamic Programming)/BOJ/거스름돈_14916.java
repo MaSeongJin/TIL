c277a33abe118834c8b65a5ada9ffde08a7c6d9a
MaSeongJin <73871625+MaSeongJin@users.noreply.github.com>
2024-02-16 18:11:08 +0900

    Create 거스름돈_14916.java

---
+++ algorithm/동적프로그래밍(Dynamic Programming)/BOJ/거스름돈_14916.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static int N;
+	public static int[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+
+		dp = new int[100001];
+		dp[2] = 1;
+		dp[4] = 2;
+		dp[5] = 1;
+
+		for (int i = 6; i <= N; i++) {
+			if (dp[i - 2] == 0 && dp[i - 5] == 0) {
+				dp[i] = 0;
+			} else if (dp[i - 2] > 0 && dp[i - 5] == 0) {
+				dp[i] = dp[i - 2] + 1;
+			} else if (dp[i - 2] == 0 && dp[i - 5] > 0) {
+				dp[i] = dp[i - 5] + 1;
+			} else {
+				dp[i] = Math.min(dp[i - 2], dp[i - 5]) + 1;
+			}
+		}
+
+		if (dp[N] == 0)
+			System.out.println("-1");
+		else
+			System.out.println(dp[N]);
+
+	}
+}
