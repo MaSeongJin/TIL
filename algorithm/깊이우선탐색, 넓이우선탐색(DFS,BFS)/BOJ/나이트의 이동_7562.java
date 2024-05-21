ce57416ba746c07f5a2cbbefcf30f73d8613dad6
MaSeongJin <making1104@naver.com>
2024-05-21 22:24:22 +0900

    Create 나이트의 이동_7562.java

---
+++ algorithm/깊이우선탐색, 넓이우선탐색(DFS,BFS)/BOJ/나이트의 이동_7562.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};
+    static int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
+    static int[][] map;
+    static boolean[][] visit;
+    static Queue<int[]> q = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            q = new LinkedList<>();
+            int I = Integer.parseInt(br.readLine());
+            map = new int[I][I];
+            visit = new boolean[I][I];
+            StringTokenizer tk = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(tk.nextToken());
+            int y1 = Integer.parseInt(tk.nextToken());
+            tk = new StringTokenizer(br.readLine());
+            int x2 = Integer.parseInt(tk.nextToken());
+            int y2 = Integer.parseInt(tk.nextToken());
+
+            q.offer(new int[]{x1, y1});
+            visit[x1][y1] = true;
+
+            while (!q.isEmpty()) {
+                int[] temp = q.poll();
+                int x = temp[0];
+                int y = temp[1];
+
+                if (x == x2 && y == y2)
+                    break;
+
+                for (int dir = 0; dir < 8; dir++) {
+                    int nx = x + dx[dir];
+                    int ny = y + dy[dir];
+
+                    if (nx < 0 || ny < 0 || nx >= I || ny >= I) continue;
+                    if (visit[nx][ny] || map[nx][ny] != 0) continue;
+                    map[nx][ny] = map[x][y] + 1;
+                    q.offer(new int[]{nx, ny});
+                }
+            }
+            System.out.println(map[x2][y2]);
+        }
+    }
+}
