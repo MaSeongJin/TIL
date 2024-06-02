9fd9b2684aada55eee01377d34866f0a901d1f50
MaSeongJin <making1104@naver.com>
2024-06-02 18:07:42 +0900

    Update 투에-모스 문자열_18222.java

--- algorithm/분할정복(Divide and Conquer)/BOJ/투에-모스 문자열_18222.java
+++ algorithm/분할정복(Divide and Conquer)/BOJ/투에-모스 문자열_18222.java
@@ -9,8 +9,7 @@ public class Main {
         long K = Long.parseLong(br.readLine());
         map = new long[65];
         map[0] = 1;
-        for(int i = 1; i < 65; ++i)
-        {
+        for(int i = 1; i < 65; ++i) {
             map[i] = map[i-1] * 2;
         }
         System.out.print(get(K));
@@ -19,9 +18,9 @@ public class Main {
     public static long get(long k) {
         if(k == 1) return 0;
 
-        for(int i = 0; i < 65; ++i)
-        {
-            if(k <= map[i]) return 1- get(k - map[i-1]);
+        for(int i = 0; i < 65; ++i) {
+            if(k <= map[i]) 
+                return 1- get(k - map[i-1]);
         }
         return 0;
     }
