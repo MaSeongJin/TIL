e52cb3b058188438ab132bea101c9ac27134717b
MaSeongJin <making1104@naver.com>
2024-08-29 17:55:32 +0900

    Create A와 B 2_12919.java

---
+++ algorithm/문자열(String)/BOJ/A와 B 2_12919.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+    static String S, T;
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        S = br.readLine();
+        T = br.readLine();
+
+        ans = 0;
+        makeS(T);
+
+        System.out.println(ans);
+    }
+
+    private static void makeS(String t) {
+        int len = t.length();
+
+        if (len == S.length()) {
+            if (t.equals(S)) {
+                ans = 1;
+            }
+            return;
+        }
+
+        if (t.endsWith("A")) {
+            makeS(t.substring(0, len - 1));
+        }
+
+        if (t.startsWith("B")) {
+            makeS(new StringBuilder(t.substring(1)).reverse().toString());
+        }
+    }
+}
