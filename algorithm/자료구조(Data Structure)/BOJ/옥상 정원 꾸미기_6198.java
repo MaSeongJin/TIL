872a881fa60be1b8cba81bdf1e30e85fa9413706
MaSeongJin <making1104@naver.com>
2024-10-18 16:23:32 +0900

    Create 옥상 정원 꾸미기_6198.java

---
+++ algorithm/자료구조(Data Structure)/BOJ/옥상 정원 꾸미기_6198.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static Stack<Integer> stack = new Stack<>();
+	public static long ans;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			
+			int height = Integer.parseInt(br.readLine());
+			
+			while(!stack.isEmpty()) {
+				
+				if(stack.peek() <= height) {
+					stack.pop();
+				}
+				else break;
+			}
+			ans += stack.size();
+			stack.push(height);
+		}
+		bw.write(String.valueOf(ans));
+		bw.close();
+	}
+}
